aa97dbd8f13135e6570ae009250602a7560f967c
Morgan Lieberthal <morgan@jmorgan.org>
2015-03-18 21:14:32 -0600

    actually migrated

--- db/migrate/20150319025708_create_seasons.rb
+++ db/migrate/20150319025708_create_seasons.rb
@@ -7,14 +7,14 @@ class CreateSeasons < ActiveRecord::Migration
 		create_table :games do |t|
 			t.belongs_to :season, index:true
 			t.integer :game_number, limit: 4, null: false
-			t.integer :gcode, limit: 5, null: false
+			t.integer :gcode, limit: 5, null: false, index:true
 			t.integer :status, limit: 1, null: false
-			t.string :home_team, limit: 3, null: false
-			t.string :away_team, limit: 3, null: false
+			t.string :home_team, limit: 3, null: false, index:true
+			t.string :away_team, limit: 3, null: false, index:true
 			t.integer :fscore_home, limit: 2, null: false
 			t.integer :fscore_away, limit: 2, null: false
-			t.datetime :game_start, null: false
-			t.datetime :game_end, null: false
+			t.datetime :game_start, null: false, index: true
+			t.datetime :game_end, null: false, index:true
 			t.integer :periods, limit: 1, null: false
 		end
 	end
