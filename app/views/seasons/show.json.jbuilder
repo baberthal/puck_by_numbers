52aa21528f74a3072f08cf7b876467a197af7c0a
Morgan Lieberthal <morgan@jmorgan.org>
2015-03-18 21:12:06 -0600

    add scaffold for seasons, teams, games

---
+++ app/views/seasons/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @season, :id, :season_years, :created_at, :updated_at
---
+++ db/migrate/20150319025708_create_seasons.rb
@@ -0,0 +1,21 @@
+class CreateSeasons < ActiveRecord::Migration
+  def change
+    create_table :seasons do |t|
+			t.integer :season_years, limit: 8, null: false, unsigned: true
+    end
+
+		create_table :games do |t|
+			t.belongs_to :season, index:true
+			t.integer :game_number, limit: 4, null: false
+			t.integer :gcode, limit: 5, null: false
+			t.integer :status, limit: 1, null: false
+			t.string :home_team, limit: 3, null: false
+			t.string :away_team, limit: 3, null: false
+			t.integer :fscore_home, limit: 2, null: false
+			t.integer :fscore_away, limit: 2, null: false
+			t.datetime :game_start, null: false
+			t.datetime :game_end, null: false
+			t.integer :periods, limit: 1, null: false
+		end
+	end
+end
