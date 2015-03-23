f278fa1a59a91dd501bddc705ef83f2bf7ad00c6
Morgan Lieberthal <morgan@jmorgan.org>
2015-03-22 18:17:08 -0600

    fixing game summary model

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,12 +18,12 @@ class Game < ActiveRecord::Base
 			game_summary.home_fenwick_against_tot = game_summary.away_fenwick_for_tot
 			game_summary.away_fenwick_against_tot = game_summary.home_fenwick_for_tot
 
-			game_summary.home_corsi_for_evens = Game.events.event_type.where( :event_type => ["BLOCK","GOAL", "MISS", "SHOT"], :event_team_id => Game.home_team, Game.home_skaters == 6, Game.away_skaters == 6 ).sum
-			game_summary.away_corsi_for_evens = Game.events.event_type.where( :event_type => ["BLOCK","GOAL", "MISS", "SHOT"], :event_team_id => Game.away_team, Game.home_skaters == 6, Game.away_skaters == 6 ).sum
+			game_summary.home_corsi_for_evens = Game.events.event_type.where( :event_type => ["BLOCK","GOAL", "MISS", "SHOT"], :event_team_id => Game.home_team, Game.home_skaters => 6, Game.away_skaters => 6 ).sum
+			game_summary.away_corsi_for_evens = Game.events.event_type.where( :event_type => ["BLOCK","GOAL", "MISS", "SHOT"], :event_team_id => Game.away_team, Game.home_skaters => 6, Game.away_skaters => 6 ).sum
 			game_summary.home_corsi_against_evens = game_summary.away_corsi_for_evens
 			game_summary.away_corsi_against_evens = game_summary.home_corsi_for_evens
-			game_summary.home_fenwick_for_evens = Game.events.event_type.where( :event_type => ["GOAL", "MISS", "SHOT"], :event_team_id => Game.home_team, Game.home_skaters == 6, Game.away_skaters == 6 ).sum
-			game_summary.away_fenwick_for_evens = Game.events.event_type.where( :event_type => ["GOAL", "MISS", "SHOT"], :event_team_id => Game.away_team, Game.home_skaters == 6, Game.away_skaters == 6 ).sum
+			game_summary.home_fenwick_for_evens = Game.events.event_type.where( :event_type => ["GOAL", "MISS", "SHOT"], :event_team_id => Game.home_team, Game.home_skaters => 6, Game.away_skaters => 6 ).sum
+			game_summary.away_fenwick_for_evens = Game.events.event_type.where( :event_type => ["GOAL", "MISS", "SHOT"], :event_team_id => Game.away_team, Game.home_skaters => 6, Game.away_skaters => 6 ).sum
 			game_summary.home_fenwick_against_evens = game_summary.away_fenwick_for_evens
 			game_summary.away_fenwick_against_evens = game_summary.home_fenwick_for_evens
 			game_summary.home_sc_for
